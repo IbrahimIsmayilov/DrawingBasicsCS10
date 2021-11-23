239f9be48aedc527a060ca9e173b603f5a21ab00
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-22 23:15:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,32 @@ ctx.fillRect(225, 50, 50, 50); // Draw a filled square
 // DRAW TEXT
 ctx.font = "42px Comic Sans MS";
 ctx.fillStyle = "red";
-ctx.fillText("Hello Canvas!", 300, 100);
+ctx.fillText("Hello Canvas!", 300, 100); // Filled Text
 
-ctx.font = "30px Arial"; 
-ctx.strokeStyle = "blue"
+ctx.font = "30px Arial";
+ctx.strokeStyle = "blue";
+ctx.strokeText("Hello Canvas!", 350, 150); // Outlined 
+
+// DRAW LINES
+ctx.lineWidth = 4;
+ctx.strokeStyle = "orange";
+ctx.beginPath();
+ctx.moveTo(500, 200);
+ctx.lineTo(700, 150);
+ctx.lineTo(600, 100);
+ctx.stroke(); // Draw the specified path
+
+// DRAW POLYGON
+ctx.fillStyle = "cyan";
+ctx.beginPath();
+ctx.moveTo(500, 400);
+ctx.lineTo(700, 350);
+ctx.lineTo(600, 300);
+ctx.closePath(); // Go back to start of path
+ctx.fill(); // Draw the specified path
+
+// CIRCLES
+ctx.lineWidth = 5;
+ctx.strokeStyle = "#0000FF";
+
+// left off at 12:30 at the second drawing vid
